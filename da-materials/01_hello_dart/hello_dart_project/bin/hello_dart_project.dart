a38e0a3bc76cc38a8f30e9a23763d212a20350e2
Fábio Mendes <fabiomendesilva@gmail.com>
2022-07-10 12:56:37 +0000

    Removed argumensts to simplify things

--- da-materials/01_hello_dart/hello_dart_project/bin/hello_dart_project.dart
+++ da-materials/01_hello_dart/hello_dart_project/bin/hello_dart_project.dart
@@ -1,5 +1,5 @@
 import 'package:hello_dart_project/hello_dart_project.dart' as hello_dart_project;
 
-void main(List<String> arguments) {
-  print('Hello world: ${hello_dart_project.calculate()}!');
+void main() {
+  print('Hello, Dart project!');
 }
