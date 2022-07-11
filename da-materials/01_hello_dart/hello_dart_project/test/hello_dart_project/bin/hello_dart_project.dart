d48fe640fd2843933aa395eb1f95c6f39bb1f0ec
Fábio Mendes <fabiomendesilva@gmail.com>
2022-07-11 03:48:47 +0000

    Success in continuing to follow the book

--- da-materials/01_hello_dart/hello_dart_project/test/hello_dart_project/bin/hello_dart_project.dart
+++ da-materials/01_hello_dart/hello_dart_project/test/hello_dart_project/bin/hello_dart_project.dart
@@ -1,5 +1,6 @@
-import 'package:hello_dart_project/hello_dart_project.dart' as hello_dart_project;
+import 'package:hello_dart_project/hello_dart_project.dart'
+    as hello_dart_project;
 
 void main(List<String> arguments) {
-  print('Hello world: ${hello_dart_project.calculate()}!');
+  print('Hello, Dart project: ${hello_dart_project.calculate()}!');
 }
