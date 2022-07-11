2471f49c3428059499d25c3545a7db21cd98a454
Fábio Mendes <fabiomendesilva@gmail.com>
2022-07-11 18:41:16 +0000

    Fix duplicated project folder

--- da-materials/01_hello_dart/hello_dart_project/test/hello_dart_project/bin/hello_dart_project.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hello_dart_project/hello_dart_project.dart'
-    as hello_dart_project;
-
-void main(List<String> arguments) {
-  print('Hello, Dart project: ${hello_dart_project.calculate()}!');
-}
