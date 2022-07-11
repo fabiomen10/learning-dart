e18c4ef30d2d11eb0b084ac830b10dc177b689ab
Fábio Mendes <fabiomendesilva@gmail.com>
2022-07-11 21:35:47 +0000

    put comments and printing

---
+++ da-materials/02_expressions_variables_constants/code.dart
@@ -0,0 +1,26 @@
+// This is a comment. It is not executed.
+
+// This is also a comment,
+// over multiple lines.
+
+/* This is also a comment. Over many...
+many...
+many line. */
+
+/* This is a comment.
+/* And inside it is
+another comment. */
+Back to the first. */
+
+/// Documentation comments
+/// I am a documentation comment
+/// at your service.
+
+/**
+ * Me, too!
+ */
+
+void main() {
+  print('Hello, Dart!');
+  print('Hello, Dart Apprentice reader!');
+}
