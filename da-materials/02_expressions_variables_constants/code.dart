6bc168ef8d52dc84c05db9747750ec0ffe4d7399
Fábio Mendes <fabiomendesilva@gmail.com>
2022-07-13 01:09:57 -0400

    A some of math codes

--- da-materials/02_expressions_variables_constants/code.dart
+++ da-materials/02_expressions_variables_constants/code.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 // This is a comment. It is not executed.
 
 // This is also a comment,
@@ -24,3 +25,41 @@ void main() {
   print('Hello, Dart!');
   print('Hello, Dart Apprentice reader!');
 }
+
+if (someCondition) {
+  // code block
+}
+// Expressions
+print(42);
+print(3 + 2);
+print('Hello, Dart Apprentice reader!');
+print(x);
+
+// Simple arithmetics operations
+print(2 + 6);
+print(10 - 2);
+print(2 * 4);
+print(24 / 3);
+
+print(2 + 6);
+print(2+6);
+
+// Decimal numbers
+print(22 / 7);
+print(22 ^/ 7);
+
+// The Euclidean modulo operation
+print(28 % 10);
+
+// Order of operations
+print((8000 / (5*10)) - 32) ~/ (29%5);
+print(350 / 5 + 2);
+print(350 / (5 +2));
+
+// Math functions
+print(sin(45 * pi / 180));
+print(cos(135 * pi / 180));
+print(sqrt(2));
+print(max(5, 10));
+print(min(-5, -10));
+print(max(sqrt(2), pi / 2));
