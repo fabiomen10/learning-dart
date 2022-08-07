b587c3b62058d77267652da0272bee5df1951f73
fabiomen10 <fabiomendesilva@gmail.com>
2022-08-07 13:39:48 -0400

    Added Dart Track of Exercism.org

---
+++ da-materials/02_expressions_variables_constants/mini_exercise.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+void main() {
+/// Arithmetic operations: Mini-exercise
+///
+/// Did you know that the sine of `45°` equals `1` over the square root
+/// of `2`? Try it out!
+
+// Remember to add the `dart:math` import at top of the file.
+print(sin(45 * pi / 180));
+print(1 / sqrt(2));
+}
